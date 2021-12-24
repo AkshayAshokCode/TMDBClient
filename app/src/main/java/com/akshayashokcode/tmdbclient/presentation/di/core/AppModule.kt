e57f39f3b223e2777dfe0020520087f4453fbc29
Riderpool1 <akshayashokan1054.com>
2021-12-24 20:49:44 +0530

    di modules

--- app/src/main/java/com/akshayashokcode/tmdbclient/presentation/di/core/AppModule.kt
+++ app/src/main/java/com/akshayashokcode/tmdbclient/presentation/di/core/AppModule.kt
@@ -1,16 +1,23 @@
 package com.akshayashokcode.tmdbclient.presentation.di.core
 
 import android.content.Context
+import com.akshayashokcode.tmdbclient.presentation.di.artist.ArtistSubComponent
+import com.akshayashokcode.tmdbclient.presentation.di.movie.MovieSubComponent
+import com.akshayashokcode.tmdbclient.presentation.di.tvshow.TvShowSubComponent
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
-@Module
+@Module(
+    subcomponents = [MovieSubComponent::class,
+        TvShowSubComponent::class,
+        ArtistSubComponent::class]
+)
 class AppModule(private val context: Context) {
 
     @Singleton
     @Provides
-    fun provideApplicationContext():Context{
+    fun provideApplicationContext(): Context {
         return context.applicationContext
     }
 }
--- app/src/main/java/com/akshayashokcode/tmdbclient/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/akshayashokcode/tmdbclient/presentation/movie/MovieActivity.kt
@@ -3,13 +3,29 @@ package com.akshayashokcode.tmdbclient.presentation.movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.akshayashokcode.tmdbclient.R
 import com.akshayashokcode.tmdbclient.databinding.ActivityMovieBinding
+import com.akshayashokcode.tmdbclient.presentation.di.Injector
+import javax.inject.Inject
 
 class MovieActivity : AppCompatActivity() {
+    @Inject
+    lateinit var factory: MoviesViewModelFactory
+    private lateinit var movieViewModel: MovieViewModel
     private lateinit var binding:ActivityMovieBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=DataBindingUtil.setContentView(this,R.layout.activity_movie)
+        (application as Injector).createMovieSubComponent()
+            .inject(this)
+
+        movieViewModel=ViewModelProvider(this,factory)
+            .get(MovieViewModel::class.java)
+        val reponseLiveData=movieViewModel.getMovies()
+        reponseLiveData.observe(this, Observer {
+
+        })
     }
 }
