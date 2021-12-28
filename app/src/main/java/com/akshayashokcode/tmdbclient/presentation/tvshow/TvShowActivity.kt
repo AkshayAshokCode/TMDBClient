25fe5edb3ca413528c1a2931dd0960bf372e3208
Riderpool1 <akshayashokan1054.com>
2021-12-28 13:05:35 +0530

    presentation layer

--- app/src/main/java/com/akshayashokcode/tmdbclient/presentation/tvshow/TvShowActivity.kt
+++ app/src/main/java/com/akshayashokcode/tmdbclient/presentation/tvshow/TvShowActivity.kt
@@ -2,14 +2,85 @@ package com.akshayashokcode.tmdbclient.presentation.tvshow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.akshayashokcode.tmdbclient.R
 import com.akshayashokcode.tmdbclient.databinding.ActivityTvShowBinding
+import com.akshayashokcode.tmdbclient.presentation.di.Injector
+import com.akshayashokcode.tmdbclient.presentation.movie.MovieAdapter
+import javax.inject.Inject
 
 class TvShowActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityTvShowBinding
+    @Inject
+    lateinit var factory: TvShowViewModelFactory
+    private lateinit var tvShowViewModel: TvShowViewModel
+    private lateinit var adapter: TvShowAdapter
+    private lateinit var binding: ActivityTvShowBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=DataBindingUtil.setContentView(this,R.layout.activity_tv_show)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_tv_show)
+        (application as Injector).createTvShowSubComponent()
+            .inject(this)
+
+        tvShowViewModel=ViewModelProvider(this,factory)[TvShowViewModel::class.java]
+        initRecyclerview()
+    }
+    private fun initRecyclerview() {
+        binding.tvShowRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = TvShowAdapter()
+        binding.tvShowRecyclerView.adapter = adapter
+        displayPopularTvShows()
+    }
+
+    private fun displayPopularTvShows() {
+        binding.tvShowProgressBar.visibility = View.VISIBLE
+        val responseLiveData = tvShowViewModel.getTvShows()
+        responseLiveData.observe(this, Observer {
+            if (it != null) {
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.tvShowProgressBar.visibility = View.GONE
+            } else {
+                binding.tvShowProgressBar.visibility = View.GONE
+                Toast.makeText(this, "No data available", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.update, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_update -> {
+                updateTvShows()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun updateTvShows() {
+        binding.tvShowProgressBar.visibility = View.VISIBLE
+        val response = tvShowViewModel.updateTvShows()
+        response.observe(this, {
+            if (it != null) {
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.tvShowProgressBar.visibility = View.GONE
+            } else {
+                binding.tvShowProgressBar.visibility = View.GONE
+            }
+        })
     }
 }
